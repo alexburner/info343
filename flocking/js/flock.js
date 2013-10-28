0c33480b2c16b4c4209634b4c538ba1fb735a2ee
Alex Burner <alex.burner@gmail.com>
2013-10-28 10:36:31 -0700

    tweakz

--- flocking/js/flock.js
+++ flocking/js/flock.js
@@ -7,25 +7,40 @@
 
 	/*
 	
+	precendents
+
 	Boids
 	http://www.red3d.com/cwr/boids/
 
 	BREVE SWARM
 	http://faculty.hampshire.edu/lspector/pubs/spector-gecco2003.pdf
 	
-	 */
+	*/
+
+
 	
 
+
+
+
+
+
+
+	// maths
+
+	function coinFlip() {
+		return Math.random() > 0.5;
+	}
 
 	function getHypotenuse(x, y) {
 		return Math.sqrt(x * x + y * y);
 	}
 
 	function getNormalizedVectorSum(dx1, dy1, dx2, dy2) {
-		dv1 = getHypotenuse(dx1, dy1);
-		dxSum = dx1 + dx2;
-		dySum = dy1 + dy2;
-		dvSum = getHypotenuse(dxSum, dySum);
+		var dv1 = getHypotenuse(dx1, dy1);
+		var dxSum = dx1 + dx2;
+		var dySum = dy1 + dy2;
+		var dvSum = getHypotenuse(dxSum, dySum);
 		var normalize = dv1 / dvSum;
 		return {
 			x: dxSum * normalize,
@@ -38,17 +53,22 @@
 
 
 
-
-	var win = {};
-	win.width = window.innerWidth;
-	win.height = window.innerHeight;
+	// container
+	var container = {};
+	container.width = window.innerWidth;
+	container.height = window.innerHeight;
+	container.el = document.createElement('div');
+	container.el.setAttribute('id', 'container');
+	document.body.appendChild(container.el);
 	addEvent(window, 'resize', function (e) {
-		win.width = window.innerWidth;
-		win.height = window.innerHeight;
+		container.width = window.innerWidth;
+		container.height = window.innerHeight;
 	});
 
 
 
+
+	// keyboard
 	addEvent(window, 'keydown', function (e) {
 		e = e || window.event;
 		switch (e.keyCode) {
@@ -56,12 +76,66 @@
 				e.preventDefault();
 				frameHandler.togglePause();
 				break;
-			case 37:
-			case 38:
-			case 39:
-			case 40:
-				e.preventDefault();
+		}
+	});
+
+
+
+
+
+	// cursor
+
+	var cursor = {};
+	cursor.x = -1000;
+	cursor.y = -1000;
+
+	// state
+	cursor.NEUTRAL = 1;
+	cursor.THREAT = 2;
+	cursor.FOOD = 3;
+	cursor.is = null;
+	cursor.set = function (status) {
+		switch (status) {
+			case cursor.NEUTRAL:
+				cursor.is = cursor.NEUTRAL;
+				cursor.el.setAttribute('class', 'neutral');
 				break;
+			case cursor.THREAT:
+				cursor.is = cursor.THREAT;
+				cursor.el.setAttribute('class', 'threat');
+				break;
+			case cursor.FOOD:
+				cursor.is = cursor.FOOD;
+				cursor.el.setAttribute('class', 'food');
+				break;
+		}
+	}
+
+	// element
+	cursor.el = document.createElement('div');
+	container.el.appendChild(cursor.el, null);
+
+	// start as food
+	cursor.set(cursor.FOOD);
+
+	// move with mouse
+	addEvent(window, 'mousemove', function (e) {
+		e = e || window.event;
+		cursor.x = e.pageX;
+		cursor.y = e.pageY;
+		cursor.el.style.left = cursor.x + 'px';
+		cursor.el.style.top = cursor.y + 'px';
+	});
+
+	// toggle threat/food
+	addEvent(window, 'mouseup', function (e) {
+		e = e || window.event;
+		if (cursor.is === cursor.FOOD) {
+			cursor.set(cursor.THREAT);
+		} else if (cursor.is === cursor.THREAT) {
+			cursor.set(cursor.NEUTRAL);
+		} else if (cursor.is === cursor.NEUTRAL) {
+			cursor.set(cursor.THREAT);
 		}
 	});
 
@@ -72,13 +146,14 @@
 
 
 
+	// the things
 
 
 	var nodes = [];
 
 
 	function Node() {
-		// tracking
+		// reference
 		this.index = nodes.length;
 		nodes.push(this);
 		// size
@@ -87,13 +162,14 @@
 		this.radius.bubble = 50;
 		this.radius.neighborhood = 250;
 		// coords
-		this.x = Math.random() * win.width;
-		this.y = Math.random() * win.height;
+		this.x = Math.random() * container.width;
+		this.y = Math.random() * container.height;
 		// delta
-		this.dx = Math.random() * 10;
-		this.dy = Math.random() * 10;
-		if (Math.random() > 0.5) {this.dx *= -1;}
-		if (Math.random() > 0.5) {this.dy *= -1;}
+		this.dmax = 10;
+		this.dx = Math.random() * 5;
+		this.dy = Math.random() * 5;
+		if (coinFlip()) {this.dx *= -1;}
+		if (coinFlip()) {this.dy *= -1;}
 		// elements
 		var node = document.createElement('div');
 		var body = document.createElement('div');
@@ -108,7 +184,7 @@
 		node.appendChild(nbhd);
 		this.el = node;
 		this.stylePosition();
-		document.body.insertBefore(this.el, null);
+		container.el.appendChild(this.el, null);
 	}
 
 	Node.prototype.movePosition = function() {
@@ -121,22 +197,22 @@
 		// y teleport
 		if (this.y < 0 - this.radius.neighborhood) {
 			// top edge
-			this.y = win.height + this.radius.neighborhood;
-			this.x = win.width - this.x;
-		} else if (this.y > win.height + this.radius.neighborhood) {
+			this.y = container.height + this.radius.neighborhood;
+			this.x = container.width - this.x;
+		} else if (this.y > container.height + this.radius.neighborhood) {
 			// bottom edge
 			this.y = 0 - this.radius.neighborhood;
-			this.x = win.width - this.x;
+			this.x = container.width - this.x;
 		}
 		// x teleport
 		if (this.x < 0 - this.radius.neighborhood) {
 			// left edge
-			this.x = win.width + this.radius.neighborhood;
-			this.y = win.height - this.y;
-		} else if (this.x > win.width + this.radius.neighborhood) {
+			this.x = container.width + this.radius.neighborhood;
+			this.y = container.height - this.y;
+		} else if (this.x > container.width + this.radius.neighborhood) {
 			// right edge
 			this.x = 0 - this.radius.neighborhood;
-			this.y = win.height - this.y;
+			this.y = container.height - this.y;
 		}
 	};
 
@@ -158,13 +234,11 @@
 			if (distance < this.radius.neighborhood) {
 				if (distance < this.radius.bubble) {
 					// avoid neighbor
-					var adx = this.x - that.x;
-					var ady = this.y - that.y;
 					var nvs = getNormalizedVectorSum(
 						this.dx,
 						this.dy,
-						adx / ((this.radius.bubble / distance) * 10),
-						ady / ((this.radius.bubble / distance) * 10)
+						xDiff / ((this.radius.bubble / distance) * 10),
+						yDiff / ((this.radius.bubble / distance) * 10)
 					);
 					this.dx = nvs.x;
 					this.dy = nvs.y;
@@ -202,36 +276,105 @@
 		}
 	};
 
+	Node.prototype.handleCursor = function () {
+		switch (cursor.is) {
+			case cursor.THREAT:
+				this.avoidCursor();
+				break;
+			case cursor.FOOD:
+				this.seekCursor();
+				break;
+		}
+	};
+
+	Node.prototype.avoidCursor = function () {
+		var xDiff = this.x - cursor.x;
+		var yDiff = this.y - cursor.y;
+		var distance = getHypotenuse(xDiff, yDiff);
+		if (distance < this.radius.neighborhood) {
+			var nvs = getNormalizedVectorSum(
+				this.dx,
+				this.dy,
+				xDiff / ((this.radius.neighborhood / distance) * 10),
+				yDiff / ((this.radius.neighborhood / distance) * 10)
+			);
+			this.dx = nvs.x;
+			this.dy = nvs.y;
+		}
+	};
+
+	Node.prototype.seekCursor = function () {
+		var xDiff = this.x - cursor.x;
+		var yDiff = this.y - cursor.y;
+		
+		// avoid -> seek
+		xDiff *= -1;
+		yDiff *= -1;
+		
+		var distance = getHypotenuse(xDiff, yDiff);
+		if (distance < this.radius.neighborhood) {
+			var nvs = getNormalizedVectorSum(
+				this.dx,
+				this.dy,
+				xDiff / ((this.radius.neighborhood / distance) * 10),
+				yDiff / ((this.radius.neighborhood / distance) * 10)
+			);
+			this.dx = nvs.x;
+			this.dy = nvs.y;
+		}
+	};
+
 	Node.prototype.randomizeDelta = function () {
-		if (Math.random() < 0.5) {
+		// x delta
+		if (coinFlip()) {
 			var rdx = Math.random();
-			if (Math.random() < 0.5) {rdx *= -1;}
+			if (coinFlip()) {rdx *= -1;}
 			this.dx += rdx;
+			this.dx = this.limitDelta(this.dx);
 		}
-		if (Math.random() < 0.5) {
+		// y delta
+		if (coinFlip()) {
 			var rdy = Math.random();
-			if (Math.random() < 0.5) {rdy *= -1;}
+			if (coinFlip()) {rdy *= -1;}
 			this.dy += rdy;
+			this.dy = this.limitDelta(this.dy);
 		}
+	};
+
+	Node.prototype.limitDelta = function (dnum) {
+		if (dnum > this.dmax) {
+			dnum = this.dmax;
+		} else if (dnum < -this.dmax) {
+			dnum = -1 *this.dmax;
+		}
+		return dnum;
 	};
 
 	Node.prototype.animate = function() {
 		this.movePosition();
 		this.stylePosition();
 		this.handleNeighbors();
+		this.handleCursor();
 		this.randomizeDelta();
 	};
 
 
 
+
+
+
+
+
+
+
+	// create & animate
+
 	var count = 0;
-	while(count < 100) {
+	while(count < 64) {
 		var node = new Node();
 		frameHandler.addAnimation(node.animate, node);
 		count++;
 	}
-
-
 
 	frameHandler.start();
 
